4939e733bebe6bf54d0547096a174ad2bdfa9c51
bitfern <gymi.mine@gmail.com>
2017-04-05 18:15:26 +0200

    Add files via upload

--- oliver.kraft/src/BigBrother.java
+++ oliver.kraft/src/BigBrother.java
@@ -4,13 +4,17 @@
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.Material;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
 
 
 public class BigBrother implements org.bukkit.event.Listener {
@@ -58,11 +62,19 @@ public class BigBrother implements org.bukkit.event.Listener {
 
         event.getPlayer().sendMessage(ChatColor.YELLOW + "[FireWorX][BETA] Armor Equipped");
 
+        event.getPlayer().setFoodLevel(20);
+        event.getPlayer().setSaturation(20);
+
         // END of Move to Round Start when possible !!!
 
         //onlineplayers++;
 
         event.getPlayer().sendMessage(ChatColor.YELLOW + Integer.toString(Bukkit.getServer().getOnlinePlayers().toArray().length) + ChatColor.AQUA + " Players online");
+
+        // Give Pickaxe
+
+        event.getPlayer().getEquipment().setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
+        event.getPlayer().getEquipment().getItemInMainHand().setDurability((short) 1611);
 
     }
 
@@ -100,6 +112,19 @@ public class BigBrother implements org.bukkit.event.Listener {
 
     // End of Player Leave Event
 
+    //Start of Death Event
+
+    @EventHandler
+    public void onPlayerDeath (PlayerDeathEvent event) {
+
+        event.getEntity().getPlayer().sendMessage(ChatColor.AQUA + "You have been eliminated from the game. Feel Free to stick around in spectator mode or type /lobby to go back to the server lobby");
+
+        event.getEntity().getPlayer().setGameMode(GameMode.SPECTATOR);
+
+    }
+
+    //End of Death Event
+
     // Start of Tool Explosions
 
     @EventHandler
@@ -117,17 +142,26 @@ public class BigBrother implements org.bukkit.event.Listener {
         float power = 1F;
 
         // Delete item in case player dies from Explosion
-        event.getBrokenItem().setAmount(0);
-
-        // Give back broken diamond pickaxe
-        event.getPlayer().getEquipment().setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
-        event.getPlayer().getEquipment().getItemInMainHand().setDurability((short) 1611);
+        // event.getBrokenItem().setAmount(0);
 
         // Create Explosion on players position
         event.getPlayer().getWorld().createExplosion(X, Y, Z, power, false, false );
 
         // Chat Message
-        event.getPlayer().sendMessage(ChatColor.RED + "BOOM! " + ChatColor.YELLOW + "Your tool broke and exploded" );
+        event.getPlayer().sendMessage(ChatColor.RED + "BOOM! " + ChatColor.YELLOW + "Your tool exploded" );
+
+        // Keep Player Alive
+
+        event.getPlayer().setSaturation(20);
+        event.getPlayer().setHealth(20);
+        event.getPlayer().setFoodLevel(20);
+
+        // Give back broken diamond pickaxe
+        event.getPlayer().getEquipment().setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
+        event.getPlayer().getEquipment().getItemInMainHand().setDurability((short) 1611);
+        System.out.println("Replaced Pickaxe");
+
+
 
     }
 
